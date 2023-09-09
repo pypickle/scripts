5af96df92fb63e034ab03842b432163564f0f01a
Zakaria Khonchafi <44476801+zackkh@users.noreply.github.com>
2023-09-09 16:32:29 +0100

    Update get-nvm.sh

--- shell/get-nvm.sh
+++ shell/get-nvm.sh
@@ -1,4 +1,30 @@
-sudo apt install curl 
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-source ~/.profile 
+#!/bin/bash
+
+# Check the user's current shell
+user_shell=$(basename "$SHELL")
+
+# Install curl if not already installed
+if ! command -v curl &> /dev/null; then
+  sudo apt install curl -y
+fi
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+# Source the appropriate configuration file based on the user's shell
+if [ "$user_shell" = "bash" ]; then
+  source ~/.bashrc
+elif [ "$user_shell" = "zsh" ]; then
+  source ~/.zshrc
+else
+  echo "Unsupported shell: $user_shell"
+  exit 1
+fi
+
+# Install the latest LTS version of Node.js
 nvm install --lts
+
+# Set the default Node.js version
+nvm alias default node
+
+echo "Node.js and NVM setup completed."
